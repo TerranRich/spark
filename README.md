71f2e8b4019e28ab34ea1f88e5e2fcd258a81de7
Xylot Media <admin@xylot.com>
2023-07-07 18:39:29 +0000

    New structure for npm build vs gulp (which refused to work)

---
+++ README.md
@@ -0,0 +1,2 @@
+# spark
+A custom Drupal 10 theme that includes Bootstrap 5.3.x as a dependency and some starter Twig template files.
---
+++ dist/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Spark behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.spark = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
